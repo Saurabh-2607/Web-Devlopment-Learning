450bb921440273ba7d55c9cda2b6137de278d8a3
Saurabh <saurabh.nayla@gmail.com>
2024-10-06 23:55:59 +0530

    3.1 updated

--- week 3/3.1/mdw.js
+++ week 3/3.1/mdw.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get("/health-checkup", function(req, res){
     const username = req.headers.username;
-    const password = req.headers.password;
+    const password = req.headers.password;  
     const kidneyId = req.query.kidneyId;
 
     if (!(username === "saurabh" && password === "pass")) {
