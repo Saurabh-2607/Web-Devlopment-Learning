480231830793baa3919be4d7066a3314027bd19a
Saurabh <saurabh.nayla@gmail.com>
2024-10-13 01:15:43 +0530

    asiingment mongo updated

--- Week 4/04-mongo-with-jwt-auth/db/index.js
+++ Week 4/04-mongo-with-jwt-auth/db/index.js
@@ -1,31 +1,19 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://saurabhsharma:admin123@leartest.fw7j2qb.mongodb.net/Course_selling_app');
+mongoose.connect('your-mongodb-url');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
-    username: String,
-    password: String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
-    username: String,
-    password: String,
-    purchasedCourses: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Course'
-    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
-    title: String,
-    description: String,
-    imageLink: String,
-    price: Number
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
