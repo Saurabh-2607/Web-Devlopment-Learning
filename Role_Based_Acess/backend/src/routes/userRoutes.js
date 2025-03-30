d947191780c73f7ada0b262df907535d22d3e155
Saurabh-2607 <saurabh.nayla@gmail.com>
2025-03-31 01:20:10 +0530

    Add role-based access control middleware for user routes

--- Role_Based_Acess/backend/src/routes/userRoutes.js
+++ Role_Based_Acess/backend/src/routes/userRoutes.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const verifyToken = require('../middlewares/authMiddleware');
+const authRole = require('../middlewares/roleMiddleware');
 const router = express.Router();
 
 // only admin can access this route
 
-router.get('/admin', verifyToken, (req, res) => {
+router.get('/admin', verifyToken, authRole("admin"), (req, res) => {
     res.json({ message: 'Hello Admin' });
     console.log(`Admin ${req.user.username} has accessed the route`);
 });
     
 
 // both admin and manger can access this route
-router.get('/manager', (req, res) => {
+router.get('/manager', verifyToken, authRole("admin", "manager"), (req, res) => {
     res.json({ message: 'Hello manager' });
     console.log("manger has accessed the route");
-
 });
 
+
 // all can acess this route
-router.get('/user', (req, res) => {
+router.get('/user', verifyToken, authRole("admin", "manager", "user"), (req, res) => {
     res.json({ message: 'Hello user' });
     console.log("user has accessed the route");
 
