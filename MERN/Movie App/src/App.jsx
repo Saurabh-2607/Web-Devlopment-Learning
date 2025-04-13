2b93456b8b55a3b761d54dbaadb8ff4ba95015c1
Saurabh-2607 <saurabh.nayla@gmail.com>
2025-04-13 19:18:06 +0530

    feat: initialize Movie App with React, Vite, and Tailwind CSS

---
+++ MERN/Movie App/src/App.jsx
@@ -0,0 +1,20 @@
+import react from 'react'
+
+function App() {
+  return (
+    <main>
+      <div className='pattern'>
+        
+        <div className='wrapper'>
+          <header>
+            <h1>Find <Span  className="text-gradient">Movie</Span> You'll Enjoy Without the Hassle</h1>
+          </header>
+
+          <p>Search</p>
+          </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
